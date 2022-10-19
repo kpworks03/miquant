d8cd130f64d50129b0a0af236bc0db5bbf558f28
KP <kpworks03@gmail.com>
2022-10-20 00:24:52 +0700

    Ver1 started

--- miquant/scrape.py
+++ miquant/scrape.py
@@ -1,1 +1,16 @@
-print("Init")
+from datetime import datetime
+import os
+import pandas as pd
+from ast import Bytes
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+from pathlib import Path
+import numpy as np
+
+
+class Data:
+    def __init__(self, dataset, start, end):
+        self.dataset = dataset
+        self.start = start
+        self.end = end
